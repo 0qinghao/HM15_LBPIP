c0c28ba3d18448b0c41ae7318145588ab7c90f06
qhlin <shu_rin@shu.edu.cn>
2021-01-24 19:00:05 +0800

    生成 yuv LP 二进制文件

--- source/Lib/TLibCommon/TComPrediction.cpp
+++ source/Lib/TLibCommon/TComPrediction.cpp
@@ -739,7 +739,7 @@ Void TComPrediction::predIntraLumaAng(TComPattern *pcTComPattern, UInt uiDirMode
     if (uiDirMode == PLANAR_IDX)
     {
         // PLANAR 模式预测
-        xPredIntraPlanarnew(ptrSrc + sw + 1, sw, pDst, uiStride, iWidth, iHeight);
+        xPredIntraPlanar(ptrSrc + sw + 1, sw, pDst, uiStride, iWidth, iHeight);
     }
     else
     {
@@ -1105,7 +1105,7 @@ Void TComPrediction::predIntraChromaAng(Int *piSrc, UInt uiDirMode, Pel *piPred,
 
     if (uiDirMode == PLANAR_IDX)
     {
-        xPredIntraPlanarnew(ptrSrc + sw + 1, sw, pDst, uiStride, iWidth, iHeight);
+        xPredIntraPlanar(ptrSrc + sw + 1, sw, pDst, uiStride, iWidth, iHeight);
     }
     else
     {
